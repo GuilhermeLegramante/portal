8f456b958e472fc9aebc732a470d173e031c9803
Guilherme <guilhermelegramante@gmail.com>
2024-11-25 12:38:37 -0300

    .

--- resources/views/partials/login-logo-and-text.blade.php
+++ resources/views/partials/login-logo-and-text.blade.php
@@ -1,5 +1,5 @@
 <div class="login-logo">
     <img style="width:150px;" src="vendor/adminlte/dist/img/logo.jpg" alt=""><br>
-    <a style="font-weight: 350;" href="{{ route('loginView') }}">hs<strong>Contracheque</strong></a>
+    <a style="font-weight: 350;" href="{{ route('loginView') }}">hs<strong>Portal</strong></a>
     <h6>{{ session('clientName') }}</h6>
 </div>
